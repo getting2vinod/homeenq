50324200801d74e5b647332e25ed73dbc802f42e
Vinod Nair <getting2vinod@gmail.com>
2025-07-24 09:07:15 +0530

    Fixed inbound, enquiry filter in card

--- app.py
+++ app.py
@@ -18,7 +18,7 @@ print("Prefix loaded : " + route_prefix)
 
 app = Flask(__name__)
 app.secret_key = "thisismyveryloooongsecretkey"
-#app.register_blueprint(auth)
+app.register_blueprint(auth)
 
 SPREADSHEET_ID = '1Yv1gxQdbc5Aq4bo1CMnCAeVMvarltkryPzE4W96DtNw'
 RANGE_NAME = 'Home Enquiry Responses'
@@ -45,7 +45,7 @@ CSV_FILE = "output.csv"
 
 script_dir = os.path.dirname(__file__)
 
-#init(app)
+init(app)
 
 def get_sheet_service():
     creds = service_account.Credentials.from_service_account_file(
@@ -251,5 +251,5 @@ def card_view():
     return render_template("card.html", cards=cards)
 
 if __name__ == "__main__":
-    app.run(debug=True, port=7000)
-    #serve(app, host='0.0.0.0', port=7000)
+    #app.run(debug=True, port=7000)
+    serve(app, host='0.0.0.0', port=7000)
